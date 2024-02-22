7dae6905693ea8a2a0baf949d477ec256098f5fa
darthmaim <git@darthmaim.de>
2024-02-22 13:59:52 +0100

    test

--- .github/scripts/version.sh
+++ .github/scripts/version.sh
@@ -3,6 +3,6 @@ set -ex
 
 printf 'machine github.com\nlogin gw2treasures-release-bot[bot]\npassword %s\n' "$GITHUB_TOKEN" > "$HOME/.netrc"
 
-echo .git/config
+cat .git/config
 
 npx changeset version
