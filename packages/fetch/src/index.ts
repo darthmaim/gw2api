c518876ef019a5ca4b8ffa03e59ff6d4069e81cd
darthmaim <git@darthmaim.de>
2024-04-11 21:47:10 +0200

    Add `cache` option

--- packages/fetch/src/index.ts
+++ packages/fetch/src/index.ts
@@ -8,7 +8,7 @@ export function fetchGw2Api<
   Schema extends SchemaVersion = undefined
 >(
   endpoint: Url,
-  options: { schema?: Schema, signal?: AbortSignal } & OptionsByEndpoint<Url>
+  options: { schema?: Schema } & OptionsByEndpoint<Url> & FetchOptions
 ): Promise<EndpointType<Url, Schema>> {
   const url = new URL(endpoint, 'https://api.guildwars2.com/');
 
@@ -22,7 +22,7 @@ export function fetchGw2Api<
     url.searchParams.set('access_token', options.accessToken);
   }
 
-  return fetch(url, { redirect: 'manual', signal: options.signal }).then((r) => {
+  return fetch(url, { redirect: 'manual', signal: options.signal, cache: options.cache }).then((r) => {
     if(!r.ok) {
       throw new Gw2ApiError( `The GW2 API call to '${url.toString()}' returned ${r.status} ${r.statusText}.`, r);
     }
@@ -31,6 +31,14 @@ export function fetchGw2Api<
 
     return r.json();
   });
+}
+
+export type FetchOptions = {
+  /** @see https://developer.mozilla.org/en-US/docs/Web/API/AbortSignal */
+  signal?: AbortSignal,
+
+  /** @see https://developer.mozilla.org/en-US/docs/Web/API/Request/cache */
+  cache?: RequestCache,
 }
 
 export class Gw2ApiError extends Error {
