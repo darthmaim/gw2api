8faa21a00240c59785d68712cbe49439d38be1e5
darthmaim <git@darthmaim.de>
2024-04-11 13:49:49 +0200

    Add `/v2/commerce/listings` and `/v2/commerce/prices` types see #4

---
+++ .changeset/friendly-avocados-sleep.md
@@ -0,0 +1,5 @@
+---
+"@gw2api/types": patch
+---
+
+Add `/v2/commerce/listings` and `/v2/commerce/prices` types
--- packages/types/data/commerce.ts
+++ packages/types/data/commerce.ts
@@ -1,3 +1,25 @@
+type PriceDetail = {
+  quantity: number,
+  unit_price: number,
+}
+
+type ListingDetail = PriceDetail & {
+  listings: number,
+}
+
+export type Price = {
+  id: number,
+  whitelisted: boolean,
+  buys: PriceDetail[],
+  sells: PriceDetail[],
+}
+
+export type Listing = {
+  id: number,
+  buys: ListingDetail[],
+  sells: ListingDetail[],
+}
+
 export type TransactionCurrent = {
   id: number,
   item_id: number,
